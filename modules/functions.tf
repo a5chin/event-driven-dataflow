9b4f712fe042575d2fc82e5eba826c3cd9092c4a
a5chin <capricorn42551230@gmail.com>
2024-03-26 01:14:45 +0900

    fix: var

--- modules/functions.tf
+++ modules/functions.tf
@@ -14,10 +14,10 @@ resource "google_cloudfunctions2_function" "dataflow_creator" {
   }
 
   service_config {
-    max_instance_count    = 1
-    min_instance_count    = 0
-    available_memory      = "128Mi"
-    timeout_seconds       = 60
+    max_instance_count    = var.functions.max_instance_count
+    min_instance_count    = var.functions.min_instance_count
+    available_memory      = var.functions.available_memory
+    timeout_seconds       = var.functions.timeout_seconds
     environment_variables = local.environment_variables
     ingress_settings      = "ALLOW_INTERNAL_ONLY"
     service_account_email = google_service_account.gcf.email
@@ -25,8 +25,8 @@ resource "google_cloudfunctions2_function" "dataflow_creator" {
 
   event_trigger {
     trigger_region        = var.location
-    event_type            = "google.cloud.storage.object.v1.finalized"
-    retry_policy          = "RETRY_POLICY_RETRY"
+    event_type            = var.functions.event.event_type
+    retry_policy          = var.functions.event.retry_policy
     service_account_email = google_service_account.event.email
 
     event_filters {
